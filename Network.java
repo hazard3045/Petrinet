21be7bfd8c04c327dc1699f5041d92a7449015df
ppmar <omar.amine.pro@gmail.com>
2025-10-14 10:32:02 +0200

    added network contructors

--- Network.java
+++ Network.java
@@ -1,4 +1,6 @@
-Interface Petri_Network(){
+import java.util.ArrayList;
+
+Interface Petri_Network{
     fire_transitions(Transition);
     add_place(Place);
     rm_place(Place);
@@ -8,4 +10,19 @@ Interface Petri_Network(){
     rm_edge(Edge);
     change_edge(Edge,w,in,out);
     change_token(Place,int);
+}
+
+public class Network implements Petri_Network{
+    private ArrayList<place> places;
+    private ArrayList<Edge> edges;
+    private ArrayList<Transition> transitions;
+    private ArrayList<Transition> transitions_fireable;
+
+    public Network(){
+        this.places = places;
+        this.edges = edges;
+        this.transitions = transitions;
+        this.transitions_fireable = transitions_fireable;
+    }
+
 }
