7b72f057ec9c864f63421da7c46f097687da4f74
ppmar <omar.amine.pro@gmail.com>
2025-10-14 10:15:07 +0200

    added constructors

--- Place.java
+++ Place.java
@@ -1,16 +1,20 @@
 public class Place extends Vertex {
     private int tokens;
     private boolean is_empty;
-
+    public Place(int tokens){
+        super();
+        this.tokens = tokens;
+    }
     public void rm_tokens(int weight){
-        tokens -= weight;
+        this.tokens -= weight;
     }
 
     public void add_tokens(int weight){
-        tokens += weight;
+        this.tokens += weight;
     }
 
     public void empty(){
-        tokens = 0;
+        this.tokens = 0;
     }
 }
+
--- Transition.java
+++ Transition.java
@@ -1,6 +1,12 @@
 public class Transition extends Vertex{
     private boolean is_fireable;
 
+    public Transition(boolean is_fireable){
+        super();
+        this.is_fireable = is_fireable;
+    }
+    
+
     public void fire(){
     }
 
